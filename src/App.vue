3fcc76f4a8857dc3371e2d95839f1857512200d4
Denki <bardey.estelle1@gmail.com>
2025-11-25 09:36:40 +0100

    initialisation du projet plus ajout des boutons

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import MyButton from './components/MyButton.vue'
+</script>
+
+<template>
+  <DefaultLayout>
+    <nav class="header--nav">
+      <RouterLink to="/"><MyButton variant="outline">Home</MyButton></RouterLink>
+      <RouterLink to="/about"><MyButton variant="outline">About</MyButton></RouterLink>
+      <RouterLink to="/button"><MyButton variant="outline">Button</MyButton></RouterLink>
+    </nav>
+    <RouterView />
+  </DefaultLayout>
+</template>
