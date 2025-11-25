ac4e05f4a554571dba742bef66784fe4ffdb9c79
Denki <bardey.estelle1@gmail.com>
2025-11-25 16:26:42 +0100

    Mise en forme header-top

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,23 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <nav class="header__top">
-    <section class="header__TopLeft">
-      <p>Community</p>
-      <p>Books</p>
-      <p>Recipe index</p>
-      <p>Popular</p>
-    </section>
-    <section class="header__TopRight">
-      <button>
-        <!---->
-        Register
-      </button>
-      <button>
-        <!---->
-        Login
-      </button>
-    </section>
-  </nav>
+  <header>
+    <nav class="header__top">
+      <section class="header__TopLeft">
+        <p>Community</p>
+        <p>Books</p>
+        <p>Recipe index</p>
+        <p>Popular</p>
+      </section>
+      <section class="header__TopRight">
+        <button>
+          <!---->
+          Register
+        </button>
+        <button>
+          <!---->
+          Login
+        </button>
+      </section>
+    </nav>
+  </header>
 </template>
