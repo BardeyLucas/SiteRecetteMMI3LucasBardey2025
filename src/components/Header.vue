425558bbcb08520a68a5023eb29521d1e8dfff85
Denki <bardey.estelle1@gmail.com>
2025-11-25 13:31:33 +0100

    Debut du header

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <nav class="header__top">
+    <section class="header__TopLeft">
+      <p>Community</p>
+      <p>Books</p>
+      <p>Recipe index</p>
+      <p>Popular</p>
+    </section>
+  </nav>
+</template>
