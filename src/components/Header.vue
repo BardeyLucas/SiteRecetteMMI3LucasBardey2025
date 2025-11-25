e0f8e65b47f471196fbea314ec39abca0ebaf440
Denki <bardey.estelle1@gmail.com>
2025-11-25 16:53:40 +0100

    fin, mise en page, top header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,8 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import Login from '@/assets/assets/login.vue'
+import User from '@/assets/assets/user.vue'
+</script>
 <template>
   <header>
     <nav class="header__top">
@@ -10,11 +14,11 @@
       </section>
       <section class="header__TopRight">
         <button>
-          <!---->
+          <Login class="LoginIcon" />
           Register
         </button>
         <button>
-          <!---->
+          <User class="UserIcon" />
           Login
         </button>
       </section>
